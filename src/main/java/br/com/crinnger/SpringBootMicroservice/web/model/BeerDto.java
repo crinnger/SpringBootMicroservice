988fe86e14b211b64e951ebb798544011b133d6f
Crinnger <Crinnger@LAPTOP-RK8BE739.mshome.net>
2021-01-23 12:31:26 -0400

    Atualizar validacao

--- src/main/java/br/com/crinnger/SpringBootMicroservice/web/model/BeerDto.java
+++ src/main/java/br/com/crinnger/SpringBootMicroservice/web/model/BeerDto.java
@@ -1,11 +1,18 @@
 package br.com.crinnger.SpringBootMicroservice.web.model;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Version;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,8 +24,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+	
 	@Null
 	private UUID id;
+	
+	@Null
+	private Long version;
+	
+	@Null
+	private Timestamp createdDate;
+
+	@Null
+	private Timestamp lastModifiedDate;
 	
 	@NotBlank
 	@NotNull
@@ -28,6 +45,15 @@ public class BeerDto {
 	private BeerStyleEnum beerStyle;
 	
 	@Positive
+	@NotNull
 	private Long upc;   
+	
+	@NotNull
+	@Positive
+	private BigDecimal price;
+	
+	private Long minOnHand; 
+	
+	private Long quantityToBrew; 
 	 
 }
